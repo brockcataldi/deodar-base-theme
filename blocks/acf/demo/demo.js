81835cd3853f2c5d477f52b636ca38aac63c86ab
Brock <brock@offcenter.io>
2021-08-10 01:25:17 -0400

    Modified basic block js, commented out for future work

--- blocks/acf/demo/demo.js
+++ blocks/acf/demo/demo.js
@@ -1,31 +1,18 @@
 (function($){
 
-    /**
-     * initializeBlock
-     *
-     * Adds custom JavaScript to the block HTML.
-     *
-     * @date    15/4/19
-     * @since   1.0.0
-     *
-     * @param   object $block The block jQuery element.
-     * @param   object attributes The block attributes (only available when editing).
-     * @return  void
-     */
-    var initializeBlock = function( $block ) {
-        $block.find('img').doSomething();
-    }
+    // const initializeBlock = ( $block ) => {
+    //     $block.find('img').doSomething();
+    // }
 
-    // Initialize each block on page load (front end).
-    $(document).ready(function(){
-        $('.testimonial').each(function(){
-            initializeBlock( $(this) );
-        });
-    });
+    // // Initialize each block on page load (front end).
+    // $(document).ready(function(){
+    //     $('.testimonial').each(function(){
+    //         initializeBlock( $(this) );
+    //     });
+    // });
 
-    // Initialize dynamic block preview (editor).
-    if( window.acf ) {
-        window.acf.addAction( 'render_block_preview/type=testimonial', initializeBlock );
-    }
+    // if( window.acf ) {
+    //     window.acf.addAction( 'render_block_preview/type=demo', initializeBlock );
+    // }
 
 })(jQuery);
